1d0815cff96553aa174ca56521612bc2b1338b8d
dghnbsa <dat.nbsa@gmail.com>
2024-07-21 21:07:09 +0700

    .

--- scripts/dghnbsa/remini.js
+++ scripts/dghnbsa/remini.js
@@ -2,14 +2,16 @@ var banhsbao = JSON.parse($response.body);
 const headers = $request.headers;
 const ua = headers['User-Agent'] || headers['user-agent'];
 
-const remini = {id: "com.bigwinepot.nwdn.international.1y_p99_99_ft_pro" }
+const list = {
+  'remini': { id: "com.bigwinepot.nwdn.international.1y_p99_99_ft_pro" },  //Remini
+};
 
-for (const key in remini) {
+for (const key in list) {
   if (new RegExp(`^${key}`, `i`).test(ua)) {
-    banhsbao["me"]["active_subscriptions_ids"] = [remini[key].id];
+    banhsbao["me"]["active_subscriptions_ids"] = [list[key].id];
     banhsbao["me"]["active_bundle_subscriptions"] = [{
       "expiry": "2099-09-09T09:09:09+00:00",
-      "product_id": remini[key].id,
+      "product_id": list[key].id,
       "features": ["unlock"]
     }];
     banhsbao["settings"]["__identity__"]["expiration"] = "2099-09-09T09:09:09+00:00";
@@ -17,4 +19,4 @@ for (const key in remini) {
   }
 }
 
-$done({body: JSON.stringify(banhsbao)})
+$done({body: JSON.stringify(banhsbao)});
