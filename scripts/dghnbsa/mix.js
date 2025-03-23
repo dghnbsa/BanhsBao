022c742071218b8daf83344096467a0f7a5cdbc6
dghnbsa <dat.nbsa@gmail.com>
2025-03-23 11:55:04 +0700

    Update mix.js

--- scripts/dghnbsa/mix.js
+++ scripts/dghnbsa/mix.js
@@ -1,4 +1,4 @@
-var chxm1023 = JSON.parse($response.body);
+var banhsbao = JSON.parse($response.body);
 const vip = '/mix/getinfo';
 const sj = '/mix/purchase';
 const hf = '/mix/recovery';
@@ -6,7 +6,7 @@ const iap = '/iap/check-receipt';
 const purchase = {"errorCode":0,"orderList":[{"quantity":"1","product_id":"com.vstudio.MIX.subscription.auto.year.88","transaction_id":"490001516171224","original_transaction_id":"490001516171224","purchase_date":"2023-10-19 07:16:08 Etc/GMT","purchase_date_ms":"1697699768000","purchase_date_pst":"2023-10-19 00:16:08 America/Los_Angeles","original_purchase_date":"2023-10-19 07:16:08 Etc/GMT","original_purchase_date_ms":"1697699768000","original_purchase_date_pst":"2023-10-19 00:16:08 America/Los_Angeles","expires_date":"2099-09-09 09:09:09 Etc/GMT","expires_date_ms":"4092599349000","expires_date_pst":"2099-09-09 06:06:06 America/Los_Angeles","web_order_line_item_id":"490000704929786","is_trial_period":"true","is_in_intro_offer_period":"false","in_app_ownership_type":"PURCHASED"}],"autoBindingUserId":"","pendingRenewalInfo":[{"auto_renew_product_id":"com.vstudio.MIX.subscription.auto.year.88","product_id":"com.vstudio.MIX.subscription.auto.year.88","original_transaction_id":"490001516171224","auto_renew_status":"0"}]};
 
 if ($request.url.indexOf(vip) != -1){
-  chxm1023.data = {
+  banhsbao.data = {
     "autoStatus" : 1,
     "autoMobileMMType" : 2,
     "expires" : 0,
@@ -25,15 +25,15 @@ if ($request.url.indexOf(vip) != -1){
 }
 
 if ($request.url.indexOf(sj) != -1){
-  chxm1023.data = (purchase);
+  banhsbao.data = (purchase);
 }
 
 if ($request.url.indexOf(hf) != -1){
-  chxm1023.data = (purchase);
+  banhsbao.data = (purchase);
 }
 
 if ($request.url.indexOf(iap) != -1){
-  chxm1023.data = {
+  banhsbao.data = {
     "purchaseTime" : 1662685749,
     "giftVip" : 1,
     "isTrialPeriod" : 1,
@@ -47,4 +47,4 @@ if ($request.url.indexOf(iap) != -1){
     "sandbox" : 0
   };
 }
-$done({body : JSON.stringify(chxm1023)});
+$done({body : JSON.stringify(banhsbao)});
